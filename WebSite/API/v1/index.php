69b45a736f789fb43f7ba4143c620f4c76141831
NekoMimiOfficial <nekomimi@tilde.team>
2024-11-05 11:18:19 +0300

    rewrote v1 API to output json data

---
+++ WebSite/API/v1/index.php
@@ -0,0 +1,25 @@
+<?php
+$dir = 'res/'; // Set the directory path
+$files = glob($dir . '*.*'); // Get all files in the directory
+
+// Check if there are any files in the directory
+if (count($files) == 0) {
+  $response = ['error' => 'No files in "res/" directory'];
+} else {
+    // Generate a random index number
+    $randIndex = rand(0, count($files) - 1);
+    
+    // Get the random file name
+    $randFile = $files[$randIndex];
+    
+    // Display the random file name
+    $fileName = 'http://nekomimi.tilde.team/API/v1/res/' . basename($randFile);
+    $response = ['url' => $fileName, 'filename' => basename($randFile)];
+}
+
+header('Content-Type: application/json; charset=utf-8');
+$json = json_encode($response);
+echo $json;
+
+?>
+
